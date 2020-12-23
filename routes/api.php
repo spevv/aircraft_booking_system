08fa3613985b34a6543b75a16c0b3d657721bfc4
Oleksandr Spivak <spivak91@gmail.com>
2020-12-23 09:04:51 +0200

    [feature](api, route, controller): /flights/1/seats/book

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SeatsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::prefix('v1')->group(function () {
+    Route::post('/flights/1/seats/book', [SeatsController::class, 'book']);
+    Route::get('/flights/1/seats/book', [SeatsController::class, 'book']);
+});
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
