72708d01e63acb0c9e979bf664a12f6d90d474a6
Oleksandr Spivak <spivak91@gmail.com>
2020-12-23 08:53:20 +0200

    [feature](codeception): init

---
+++ tests/api/BookSitsCest.php
@@ -0,0 +1,28 @@
+<?php
+
+class BookSitsCest
+{
+    public function _before(ApiTester $I)
+    {
+        $I->haveHttpHeader('accept', 'application/json');
+        $I->haveHttpHeader('content-type', 'application/json');
+    }
+
+    // tests
+    public function tryToTest(ApiTester $I)
+    {
+    }
+
+    // tests
+    public function createUserViaAPI(ApiTester $I)
+    {
+        $I->sendPost('/flights/1/seats/book', [
+            'username' => 'test',
+            'seats_number' => 2
+        ]);
+        $I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK); // 200
+        $I->seeResponseIsJson();
+        //$I->seeResponseContains('{"result":"ok"}');
+
+    }
+}
